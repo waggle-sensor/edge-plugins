b42cc5c7fcc407d1a5b682542f3664733449c3f5
Yongho Kim <ironyonghokim@gmail.com>
2020-01-10 09:43:15 -0600

    Added Tensorflow 1.14.0 plugin base image

---
+++ plugin-tensorflow/download_tensorflow_1.14.0_armv7l.sh
@@ -0,0 +1,3 @@
+# Download Tensorflow 1.14.0 from https://github.com/lhelontra/tensorflow-on-arm
+
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.14.0-buster/tensorflow-1.14.0-cp35-none-linux_armv7l.whl
