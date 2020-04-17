bf09d1e2c0b92495c4d2934f654a30ea6a35490c
seonghapark <park708@purdue.edu>
2020-04-17 16:26:22 -0500

    Added inference

--- plugin-inference-fcn/pytorch_fcn/models/resnet/helpers.py
+++ plugin-inference-fcn/pytorch_fcn/models/resnet/helpers.py
@@ -6,7 +6,8 @@ def prepare_optim(opts, model):
                             lr=opts.cfg['lr'],
                             momentum=opts.cfg['momentum'],
                             weight_decay=opts.cfg['weight_decay'])
-    if opts.resume:
-        checkpoint = torch.load(opts.resume)
+    if opts.cfg['pretrained_net']:
+        print('opts.cfg[pretrained_net]', opts.cfg['pretrained_net'])
+        checkpoint = torch.load(opts.cfg['pretrained_net'])
         optim.load_state_dict(checkpoint['optim_state_dict'])
     return optim
